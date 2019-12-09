c6f5377d42c319d718ddcdfb72cccf37c4bd6ccc
lamweiyi99 <lam_weiyi1999@hotmail.com>
2019-12-09 16:47:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.carloancalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            val CarPrice = editTextCarPrice.text.toString()
+            val DownPayment = editTextDownPayment.text.toString()
+            val LoanPeriod = editTextLoanPeriod.text.toString()
+            val InterestRate = editTextInterestRate.text.toString()
+
+            val Loan:Double =  CarPrice.toDouble() - DownPayment.toDouble()
+            val Interest:Double = Loan * InterestRate.toDouble() * LoanPeriod.toDouble()
+            val MonthlyPayment:Double = (Loan + Interest) / LoanPeriod.toDouble() / 12
+
+            textViewLoan.text = "Loan: " + "%.2f".format(Loan)
+            textViewInterest.text = "Interest: " + "%.2f".format(Interest)
+            textViewMonthlyRepayment.text = "Monthly Payment: " + "%.2f".format(MonthlyPayment)
+
+        }
+
+        buttonReset.setOnClickListener{
+            editTextCarPrice.setText("")
+            editTextDownPayment.setText("")
+            editTextInterestRate.setText("")
+            editTextLoanPeriod.setText("")
+            textViewLoan.text = "Loan: "
+            textViewInterest.text = "Interest"
+            textViewMonthlyRepayment.text = "Monthly Payment"
+        }
+    }
+
+
+}
